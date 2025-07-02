063fcab8734aeda8b14cd9522967d51c3a6b6ece
Joshua Harrison <joshuaharrison@fireandlight.tech>
2025-07-02 17:09:34 -0500

    Initial checkin for page objects

---
+++ FaL-PageObjects/PageConditions.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+
+namespace FaL_PageObjects;
+public static class PageConditions
+{
+    public static Func<IWebDriver, IWebElement?> ElementToBeClickable(By locator)
+    {
+        return (driver) =>
+        {
+            try
+            {
+                var element = driver.FindElement(locator);
+                if (element != null && element.Enabled)
+                {
+                    return element;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (StaleElementReferenceException)
+            {
+                return null;
+            }
+        };
+    }
+
+    public static Func<IWebDriver, IWebElement?>ElementToBeClickable(IWebElement element)
+    {
+        return (driver) =>
+        {
+            try
+            {
+                if (element != null && element.Enabled)
+                {
+                    return element;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (StaleElementReferenceException)
+            {
+                return null;
+            }
+        };
+    }
+
+    public static Func<IWebDriver, IWebElement?> ElementIsVisible(By locator)
+    {
+        return (driver) =>
+        {
+            try
+            {
+                var element = driver.FindElement(locator);
+                return ElementIfVisible(element);
+            }
+            catch (StaleElementReferenceException)
+            {
+                return null;
+            }
+        };
+    }
+
+    private static IWebElement? ElementIfVisible(IWebElement element)
+    {
+        if (element != null && element.Displayed)
+        {
+            return element;
+        }
+        return null;
+    }
+}
---
+++ FaL.PageObject.Contract/IAutomationFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace FaL.PageObject.Contract;
+public interface IAutomationFactory
+{
+    IWebDriverProxy GetWebDriverProxy();
+    TConfig GetConfiguration<TConfig>() where TConfig : class;
+    IAutomationPage CreateAutomationPage<T>() where T : class;
+}
